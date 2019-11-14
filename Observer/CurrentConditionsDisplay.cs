383e4dad237fbdf0ae7c6ee2eb3267494266d107
Drew Fleming <andrew.fleming@ipsiti.com>
2019-11-14 13:05:40 -0500

    Adds observer pattern.

---
+++ Observer/CurrentConditionsDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ObserverPattern
+{
+  public class CurrentConditionsDisplay : IObserver, IDisplayElement
+  {
+    private readonly ISubject _weatherData;
+    private float _temperature;
+    private float _humidity;
+
+    public CurrentConditionsDisplay(ISubject weatherData)
+    {
+      this._weatherData = weatherData;
+      _weatherData.RegisterObserver(this);
+    }
+
+    public void Update(float temp, float humidity, float pressure)
+    {
+      this._temperature = temp;
+      this._humidity = humidity;
+      Display();
+    }
+
+    public void Display()
+    {
+      Console.WriteLine($"Current Conditions: Temperature {_temperature}. Humidity {_humidity}%");
+    }
+  }
+}
