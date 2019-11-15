54a1f09f59d6a0d783ca703c6a2245159eed7eab
Drew Fleming <andrew.fleming@ipsiti.com>
2019-11-15 11:02:50 -0500

    Adds singleton pattern.

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace SingletonPattern
+{
+  public class Singleton
+  {
+    private static Singleton _instance;
+    private static readonly object Padlock = new object();
+
+    private Singleton()
+    {
+    }
+
+    public static Singleton Instance
+    {
+      get
+      {
+        lock (Padlock)
+        {
+          return _instance ?? (_instance = new Singleton());
+        }
+      }
+    }
+  }
+}
